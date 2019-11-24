6b32e3ef7e5ecea1bd106df96b0c51b626ffeb02
Matthew <matthewandkaren@hotmail.com>
2019-11-24 19:34:44 +0000

    detect changes and trigger save if changed

--- app/src/main/java/click/mr_b/fprp_app/NewPlanActivity.java
+++ app/src/main/java/click/mr_b/fprp_app/NewPlanActivity.java
@@ -8,7 +8,9 @@ import android.content.Intent;
 import android.support.v4.media.MediaBrowserCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
@@ -17,7 +19,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 
-public class NewPlanActivity extends AppCompatActivity {
+public class NewPlanActivity extends AppCompatActivity implements TextWatcher {
 
     public static final String EXTRA_REPLY_ID = "ID";
     public static final String EXTRA_REPLY_NAME = "NAME";
@@ -46,6 +48,8 @@ public class NewPlanActivity extends AppCompatActivity {
     private EditText mEditRP3View;
     private EditText mEditRP4View;
     private EditText mEditRP5View;
+    private PlanViewModel mPlanViewModel;
+    private boolean planChanged = false;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -67,30 +71,23 @@ public class NewPlanActivity extends AppCompatActivity {
         mEditRP4View = findViewById(click.mr_b.fprp_app.R.id.edit_rp4);
         mEditRP5View = findViewById(click.mr_b.fprp_app.R.id.edit_rp5);
 
+        mEditPlanView.addTextChangedListener(this);
+        mEditQ1View.addTextChangedListener(this);
+        mEditQ2View.addTextChangedListener(this);
+        mEditQ3View.addTextChangedListener(this);
+        mEditQ4View.addTextChangedListener(this);
+        mEditQ5View.addTextChangedListener(this);
+        mEditQ6View.addTextChangedListener(this);
+        mEditRP1View.addTextChangedListener(this);
+        mEditRP2View.addTextChangedListener(this);
+        mEditRP3View.addTextChangedListener(this);
+        mEditRP4View.addTextChangedListener(this);
+        mEditRP5View.addTextChangedListener(this);
+
         final Button button = findViewById(click.mr_b.fprp_app.R.id.button_save);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                Intent replyIntent = new Intent();
-                if (TextUtils.isEmpty(mEditPlanView.getText())) {
-                    setResult(RESULT_CANCELED, replyIntent);
-                } else {
-                    String name = mEditPlanView.getText().toString();
-                    replyIntent.putExtra(EXTRA_REPLY_NAME, name);
-                    replyIntent.putExtra(EXTRA_REPLY_ID, mPlanIdView.getText().toString());
-                    replyIntent.putExtra(EXTRA_REPLY_Q1, mEditQ1View.getText().toString());
-                    replyIntent.putExtra(EXTRA_REPLY_Q2, mEditQ2View.getText().toString());
-                    replyIntent.putExtra(EXTRA_REPLY_Q3, mEditQ3View.getText().toString());
-                    replyIntent.putExtra(EXTRA_REPLY_Q4, mEditQ4View.getText().toString());
-                    replyIntent.putExtra(EXTRA_REPLY_Q5, mEditQ5View.getText().toString());
-                    replyIntent.putExtra(EXTRA_REPLY_Q6, mEditQ6View.getText().toString());
-                    replyIntent.putExtra(EXTRA_REPLY_RP1, mEditRP1View.getText().toString());
-                    replyIntent.putExtra(EXTRA_REPLY_RP2, mEditRP2View.getText().toString());
-                    replyIntent.putExtra(EXTRA_REPLY_RP3, mEditRP3View.getText().toString());
-                    replyIntent.putExtra(EXTRA_REPLY_RP4, mEditRP4View.getText().toString());
-                    replyIntent.putExtra(EXTRA_REPLY_RP5, mEditRP5View.getText().toString());
-                    setResult(RESULT_OK, replyIntent);
-                }
-                finish();
+                savePlan();
             }
         });
         Bundle bundle = intent.getExtras();
@@ -116,9 +113,14 @@ public class NewPlanActivity extends AppCompatActivity {
     @Override
     public void onBackPressed()
     {
-        Intent replyIntent = new Intent();
-        setResult(RESULT_CANCELED, replyIntent);
-        finish();
+        if (planChanged){
+            savePlan();
+        }
+        else {
+            Intent replyIntent = new Intent();
+            setResult(RESULT_CANCELED, replyIntent);
+            finish();
+        }
     }
 
     @Override
@@ -154,4 +156,44 @@ public class NewPlanActivity extends AppCompatActivity {
         Log.v(this.getClass().getSimpleName(), "- ON DESTROY -");
     }
 
+    private void savePlan(){
+        Intent replyIntent = new Intent();
+        if (TextUtils.isEmpty(mEditPlanView.getText())) {
+            setResult(RESULT_CANCELED, replyIntent);
+        } else {
+            String name = mEditPlanView.getText().toString();
+            replyIntent.putExtra(EXTRA_REPLY_NAME, name);
+            replyIntent.putExtra(EXTRA_REPLY_ID, mPlanIdView.getText().toString());
+            replyIntent.putExtra(EXTRA_REPLY_Q1, mEditQ1View.getText().toString());
+            replyIntent.putExtra(EXTRA_REPLY_Q2, mEditQ2View.getText().toString());
+            replyIntent.putExtra(EXTRA_REPLY_Q3, mEditQ3View.getText().toString());
+            replyIntent.putExtra(EXTRA_REPLY_Q4, mEditQ4View.getText().toString());
+            replyIntent.putExtra(EXTRA_REPLY_Q5, mEditQ5View.getText().toString());
+            replyIntent.putExtra(EXTRA_REPLY_Q6, mEditQ6View.getText().toString());
+            replyIntent.putExtra(EXTRA_REPLY_RP1, mEditRP1View.getText().toString());
+            replyIntent.putExtra(EXTRA_REPLY_RP2, mEditRP2View.getText().toString());
+            replyIntent.putExtra(EXTRA_REPLY_RP3, mEditRP3View.getText().toString());
+            replyIntent.putExtra(EXTRA_REPLY_RP4, mEditRP4View.getText().toString());
+            replyIntent.putExtra(EXTRA_REPLY_RP5, mEditRP5View.getText().toString());
+            setResult(RESULT_OK, replyIntent);
+        }
+        finish();
+
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+        Log.v(this.getClass().getSimpleName(), "- afterTextChanged -");
+        planChanged = true;
+    }
 }
